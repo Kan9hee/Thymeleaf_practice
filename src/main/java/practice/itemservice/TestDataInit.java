08ff08fd6becc4076266e4a93f89732890640e65
Kanghee Han <hcsy1219@gmail.com>
2023-02-27 23:39:42 +0900

    form tags

---
+++ src/main/java/practice/itemservice/TestDataInit.java
@@ -0,0 +1,22 @@
+package practice.itemservice;
+
+import practice.itemservice.domain.item.Item;
+import practice.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
